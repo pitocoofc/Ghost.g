7605bf83b89dd2229a9c5bd3cc924598ae93faf7
Ghost <ismaelkelvyn328@gmail.com>
2026-04-27 20:00:45 -0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,8 @@
+const ghostConfig = {
+    name: "Ghost",
+    avatar: "https://via.placeholder.com/150",
+    background: "https://via.placeholder.com/1920x1080",
+    music: "link_para_sua_musica.mp3",
+    ghostSearchUrl: "https://seu-link-do-ghost-search.com",
+    bio: "Developer & Creator. Construindo o futuro no terminal."
+};

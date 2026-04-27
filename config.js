20fee0391ab517beb177a988fe316bdc66e276f4
Ghost <ismaelkelvyn328@gmail.com>
2026-04-27 20:14:19 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const ghostConfig = {
     name: "Ghost",
     avatar: "https://cdn.discordapp.com/avatars/1074028627982815303/47b28985df3d85e30d9d8e12ddc39a3d.png?size=2048",
     background: "https://via.placeholder.com/1920x1080",
-    music: "Os Barões da Pisadinha - Já que me ensinou a Beber (VELTON DO GRAVE).mp3",
+    music: "https://raw.githubusercontent.com/pitocoofc/Ghost.g/main/music.mp3",
     ghostSearchUrl: "https://seu-link-do-ghost-search.com",
     bio: "Developer & Creator. Construindo o futuro no terminal."
 };

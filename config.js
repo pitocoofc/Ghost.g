6f9dd88a589561dfcc648e7915373d43b5438ccf
Ghost <ismaelkelvyn328@gmail.com>
2026-04-27 20:42:02 -0300

    Update background image URL in config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const ghostConfig = {
     name: "Ghost",
     avatar: "https://cdn.discordapp.com/avatars/1074028627982815303/47b28985df3d85e30d9d8e12ddc39a3d.png?size=2048",
-    background: "https://avatars.githubusercontent.com/u/215600808?s=400&u=c9da2895a7c4d306e6787d810896be5612bd1a7a&v=4",
+    background: "https://raw.githubusercontent.com/pitocoofc/Ghost.g/main/fundo.png",
     music: "https://raw.githubusercontent.com/pitocoofc/Ghost.g/main/music.mp3",
     ghostSearchUrl: "https://seu-link-do-ghost-search.com",
     bio: "Developer & Creator. Construindo o futuro no terminal."

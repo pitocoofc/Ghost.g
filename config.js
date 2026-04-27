9e0fd88ef801f44b178f9d113857e25b42400b5b
Ghost <ismaelkelvyn328@gmail.com>
2026-04-27 20:03:40 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const ghostConfig = {
     name: "Ghost",
-    avatar: "https://via.placeholder.com/150",
+    avatar: "https://cdn.discordapp.com/avatars/1074028627982815303/47b28985df3d85e30d9d8e12ddc39a3d.png?size=2048",
     background: "https://via.placeholder.com/1920x1080",
     music: "link_para_sua_musica.mp3",
     ghostSearchUrl: "https://seu-link-do-ghost-search.com",

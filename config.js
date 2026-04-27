0e74a8701c8416817e522ea8be8c1022de88a26c
Ghost <ismaelkelvyn328@gmail.com>
2026-04-27 20:21:50 -0300

    Update background URL in ghostConfig

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const ghostConfig = {
     name: "Ghost",
     avatar: "https://cdn.discordapp.com/avatars/1074028627982815303/47b28985df3d85e30d9d8e12ddc39a3d.png?size=2048",
-    background: "https://via.placeholder.com/1920x1080",
+    background: "https://github-readme-stats.vercel.app/api/top-langs/?username=pitocoofc&langs_count=10&layout=compact&theme=dark",
     music: "https://raw.githubusercontent.com/pitocoofc/Ghost.g/main/music.mp3",
     ghostSearchUrl: "https://seu-link-do-ghost-search.com",
     bio: "Developer & Creator. Construindo o futuro no terminal."

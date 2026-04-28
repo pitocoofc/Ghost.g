da2e611d941c746b7589e95c24f50f622b3adddc
Ghost <ismaelkelvyn328@gmail.com>
2026-04-27 21:09:21 -0300

    Update config.js

--- config.js
+++ config.js
@@ -3,6 +3,6 @@ const ghostConfig = {
     avatar: "https://cdn.discordapp.com/avatars/1074028627982815303/47b28985df3d85e30d9d8e12ddc39a3d.png?size=2048",
     background: "https://cdn.discordapp.com/avatars/1074028627982815303/47b28985df3d85e30d9d8e12ddc39a3d.png?size=2048",
     music: "https://raw.githubusercontent.com/pitocoofc/Ghost.g/main/music.mp3",
-    ghostSearchUrl: "https://raw.githubusercontent.com/pitocoofc/Ghost.g/main/P.html",
+    ghostSearchUrl: "https://pitocoofc.github.io/Ghost.g/P.html",
     bio: "Developer & Creator. Construindo o futuro no terminal."
 };
